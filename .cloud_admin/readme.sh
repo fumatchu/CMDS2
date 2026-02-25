e553fd2fb7f32acc6a6e303ecf5399fd257d39b3
Meraki Matt <matthewj.gear@gmail.com>
2026-02-25 02:36:33 -0500

    Added readme and upload release 1.0.2 for cloud

---
+++ .cloud_admin/readme.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Show CMDS README in a scrollable dialog box
+
+dialog --backtitle "CMDS-Deployment Server" \
+       --title "CMDS CLOUD Catalyst-to-Meraki README" \
+       --no-collapse \
+       --textbox ./read.me 30 120
